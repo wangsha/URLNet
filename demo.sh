ccdf8c7f66cf3fea571b606581285f72af05ac93
CircleCI <bumpversion@circleci.com>
2018-11-07 15:05:46 +0800

    parallel task

--- demo.sh
+++ demo.sh
@@ -1,34 +1,38 @@
 #!/usr/bin/env bash
-#emb_modes=(1 2 2 3 3 4 5)
-#delimit_modes=(0 0 1 0 1 1 1 )
-emb_modes=(1 2 5)
-delimit_modes=(0 1 1)
-train_size=10000
-test_size=10000
+emb_modes=(1 2 2 3 3 4 5)
+delimit_modes=(0 0 1 0 1 1 1 )
+#emb_modes=(5)
+#delimit_modes=(1)
+train_size=500000
+test_size=500000
 nb_epoch=5
 data_dir='./data'
 
 add_expert_feature=1
 
-for ((i=0; i <${#emb_modes[@]}; ++i))
-    do
+task(){
     python temp_debug.py --data.data_dir ${data_dir}/train_${train_size}.txt \
-    --data.dev_pct 0.1 --data.delimit_mode ${delimit_modes[$i]} --data.min_word_freq 1 \
+    --data.dev_pct 0.001 --data.delimit_mode ${delimit_modes[$1]} --data.min_word_freq 1 \
     --train.add_expert_feature ${add_expert_feature} \
-    --model.emb_mode ${emb_modes[$i]} --model.emb_dim 32 --model.filter_sizes 3,4,5,6 \
+    --model.emb_mode ${emb_modes[$1]} --model.emb_dim 32 --model.filter_sizes 3,4,5,6 \
     --train.nb_epochs ${nb_epoch} --train.batch_size 1048 --train.add_expert_feature=${add_expert_feature} \
     --log.print_every 5 --log.eval_every 10 --log.checkpoint_every 10 \
-    --log.output_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/
+    --log.output_dir runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/
 
     python test.py --data.data_dir ${data_dir}/test_${test_size}.txt \
-    --data.delimit_mode ${delimit_modes[$i]} \
-    --data.word_dict_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/words_dict.p \
-    --data.subword_dict_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/subwords_dict.p \
-    --data.char_dict_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/chars_dict.p \
-    --log.checkpoint_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/checkpoints/ \
-    --log.output_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/train_${train_size}_test_${test_size}.txt \
-    --model.emb_mode ${emb_modes[$i]} --model.emb_dim 32 \
+    --data.delimit_mode ${delimit_modes[$1]} \
+    --data.word_dict_dir runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/words_dict.p \
+    --data.subword_dict_dir runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/subwords_dict.p \
+    --data.char_dict_dir runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/chars_dict.p \
+    --log.checkpoint_dir runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/checkpoints/ \
+    --log.output_dir runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/train_${train_size}_test_${test_size}.txt \
+    --model.emb_mode ${emb_modes[$1]} --model.emb_dim 32 \
     --test.batch_size 1048
 
-    python auc.py --input_path runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/ --input_file train_${train_size}_test_${test_size}.txt --threshold 0.5
+    python auc.py --input_path runs/${train_size}_emb${emb_modes[$1]}_dlm${delimit_modes[$1]}_32dim_minwf1_1conv3456_${nb_epoch}ep_expert${add_expert_feature}/ --input_file train_${train_size}_test_${test_size}.txt --threshold 0.5
+}
+
+for ((i=0; i <${#emb_modes[@]}; ++i))
+    do
+        task "$i" &
     done
